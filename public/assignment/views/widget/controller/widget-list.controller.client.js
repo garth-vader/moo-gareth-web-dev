95b3435bded7196508018f0e222f6ae98a2bdda7
garth-vader <moo.g@husky.neu.edu>
2016-05-31 03:19:22 -0400

    Added image youtube heading back

--- public/assignment/views/widget/controller/widget-list.controller.client.js
+++ public/assignment/views/widget/controller/widget-list.controller.client.js
@@ -1,3 +1,29 @@
-/**
- * Created by gmoo on 5/31/16.
- */
+(function() {
+    angular
+        .module("WebAppMaker")
+        .controller("WidgetListController", WidgetListController);
+
+    function WidgetListController($routeParams, $sce, WidgetService) {
+        var vm = this;
+
+        vm.userId = $routeParams["uid"];
+        vm.websiteId = $routeParams["wid"];
+        vm.pageId = $routeParams["pid"];
+        vm.widgets = WidgetService.findWidgetsByPageId(vm.pageId);
+
+        vm.getTrustedHtml = getTrustedHtml;
+        vm.getTrustedUrl = getTrustedUrl;
+
+        function getTrustedHtml(widget) {
+            var html = $sce.trustAsHtml(widget.text);
+            return html;
+        }
+
+        function getTrustedUrl(widget) {
+            var urlParts = widget.url.split("/");
+            var id = urlParts[urlParts.length - 1];
+            var url = "https://www.youtube.com/embed/" + id;
+            return $sce.trustAsResourceUrl(url);
+        }
+    }
+})();
