4866d90b4091503756738eb6e59e87db2e956cd3
garth-vader <moo.g@husky.neu.edu>
2016-05-31 03:31:25 -0400

    added widget new controller

--- public/assignment/views/widget/controller/widget-new.controller.client.js
+++ public/assignment/views/widget/controller/widget-new.controller.client.js
@@ -1,3 +1,18 @@
-/**
- * Created by gmoo on 5/31/16.
- */
+
+(function () {
+    angular
+        .module("WebAppMaker")
+        .controller("NewWidgetController", NewWidgetController);
+
+    function NewWidgetController($routeParams, $location, WidgetService) {
+        var vm = this;
+        vm.userId = $routeParams["uid"];
+        vm.websiteId = $routeParams["wid"];
+        vm.pageId = $routeParams["pid"];
+        vm.createWidget = createWidget;
+
+        function createWidget(widgetType) {
+
+        }
+    }
+})();
