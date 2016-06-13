b958009f1374ebbecf41c76fe3e554ad0ac4dbc5
garth-vader <moo.g@husky.neu.edu>
2016-06-13 08:29:06 -0400

    openshift mongo db

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,7 +1,16 @@
 module.exports = function() {
     var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/cs4550summer1');
-    
+    var monString = 'mongodb://localhost/cs4550summer1';
+
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        monString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+    mongoose.connect(monString);
+
     var userModel = require("./users/user.model.server.js")();
     var websiteModel = require("./website/website.model.server")();
     var pageModel = require("./page/page.model.server")();
